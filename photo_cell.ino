c514c9c398906955d62852bfb92606d142de1b31
Alessandro Caianiello <alessandro@caianiello.it>
2018-04-04 21:16:41 +0100

    Use median

--- photo_cell.ino
+++ photo_cell.ino
@@ -1,28 +1,30 @@
 #include <CircularBuffer.h>
+#include "RunningMedian.h"
 
-#define BUFFER_SIZE 15
-#define PARSE_BLOCK 5
-#define TREND_SIZE 3 // cannot change
+#define SAMPLE_SIZE 3
 #define ALERT_AFTER_MINUTES 1
 #define SAFEGUARD_ALERT_AFTER_MINUTES 60
 #define BUZZER 8
-#define UPDATE_FREQUENCY 50
+#define UPDATE_FREQUENCY 20
+#define SAMPLING_FREQUENCY 200
 
-int photocellPin = 0;
+int photocellPin = A0;
 unsigned long triggerAlertAt = -1;
 unsigned long safeguardAlertAt = -1;
 bool inUse = false;
-int averageForBlocks[TREND_SIZE];
-int start, end = 0;
 
-CircularBuffer<int,BUFFER_SIZE> buffer;
+CircularBuffer<int,SAMPLE_SIZE> samples;
+RunningMedian samplesMedian = RunningMedian(10);
+unsigned long lastSampleAt = -1;
 
 void setup() {
   Serial.begin(9600);
   pinMode(BUZZER, OUTPUT);  
+  pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   triggerAlertAt = millis() + milisecondsFor(ALERT_AFTER_MINUTES);
   safeguardAlertAt = millis() + milisecondsFor(SAFEGUARD_ALERT_AFTER_MINUTES);
+  lastSampleAt = millis();
 }
 
 unsigned long milisecondsFor(long minutes){
@@ -31,17 +33,19 @@ unsigned long milisecondsFor(long minutes){
 
 void loop() { 
   int v = analogRead(photocellPin); 
-  buffer.push(v);
-  Serial.println(v);
-  int state = parseTrend();
+  samplesMedian.add(v);  
+  int state = parseSamples();
+
   switch(state){
     case -1:
-      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("LED_BUILTIN, HIGH");
+      digitalWrite(13, HIGH);
       inUse = false;
       startCounterForAlert();
       break;
     case 1:
-      digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("LED_BUILTIN, LOW");
+      digitalWrite(13, LOW);
       inUse = true;
       resetAlert();
       break;
@@ -50,8 +54,17 @@ void loop() {
   }
    checkAlertTrigger();
    checkSafeGuardAlertTrigger();
-   delay(UPDATE_FREQUENCY); 
+   delay(UPDATE_FREQUENCY);
+   addSample(samplesMedian.getMedian());
 }
+
+void addSample(long v){
+  if(millis() > lastSampleAt){
+    samples.push(v);
+    lastSampleAt = millis() + SAMPLING_FREQUENCY;
+  }
+}
+
 void startCounterForAlert(){
   triggerAlertAt = millis() + milisecondsFor(ALERT_AFTER_MINUTES);
 }
@@ -79,33 +92,19 @@ void stopBuzz(){
   safeguardAlertAt = millis() + milisecondsFor(SAFEGUARD_ALERT_AFTER_MINUTES);
 }
 
-int parseTrend(){
+int parseSamples(){
   int returnValue = 2;
-  if(buffer.size() == BUFFER_SIZE){
-    for(int i=0; i<TREND_SIZE; i++){
-      start = max(PARSE_BLOCK*i-1, 0); 
-      end = PARSE_BLOCK*(i+1)-1;
-      averageForBlocks[i] = averageFor(start, end);
-    }
-    // if the tail of the array has similar average AND there is a difference between the tail and the head
-    if(abs(averageForBlocks[2]-averageForBlocks[1])<20 && abs(averageForBlocks[0] - averageForBlocks[2]) > 20){
-      if(averageForBlocks[0] < averageForBlocks[1]){
+  if(samples.size() == SAMPLE_SIZE){
+    if(abs(samples[SAMPLE_SIZE-1]-samples[SAMPLE_SIZE-2])<20 && abs(samples[0] - samples[SAMPLE_SIZE-1]) > 20){
+      if(samples[0] < samples[SAMPLE_SIZE-1]){
         returnValue = 1;
       }else{
         returnValue = -1;
       }
     }else{
       returnValue = 2;
-    }
-  }else{
-    Serial.println("Not enough values");
+    }  
   }
   return returnValue;
-}
-
-int averageFor( int start, int end){
-  int tmp = 0;
-  for(int i = start; i<=end; i++){ tmp += buffer[i]; }
-  return tmp/PARSE_BLOCK;
 }
 
